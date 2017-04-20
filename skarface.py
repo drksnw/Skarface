721bad2fbd80797860ea71813d1978e1d2481507
Guillaume Petitpierre <guillaume.petitpierre@bluewin.ch>
2017-04-20 12:48:22 +0200

    Nose and mouth detection

--- skarface.py
+++ skarface.py
@@ -8,6 +8,9 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('parojosG.xml')
 
+nose_cascade = cv2.CascadeClassifier('Nariz.xml')
+mouth_cascade = cv2.CascadeClassifier('Mouth.xml')
+
 #img = cv2.imread("pics/dom.jpg")
 video_capture = cv2.VideoCapture(0)
 while True:
@@ -31,20 +34,38 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
 
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        eyesy = 0
+        eyesh = 0
         for (ex,ey,ew,eh) in eyes:
-            #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            eyesy = ey
+            eyesh = eh
             eh *= 2
             eyes_zone = img[y+ey:y+ey+eh, x:x+w]
             resized = cv2.resize(sunglasses, (w, eh))
             sb,sg,sr,sa = cv2.split(resized)
-            eyes_sung = cv2.bitwise_xor(resized, eyes_zone)
-            eyes_sung = cv2.add(resized, eyes_sung)
+            eyes_sung = cv2.add(resized, eyes_zone, mask=255-sa)
             img[y+ey:y+ey+eh, x:x+w] = eyes_sung
+
+        mouth = mouth_cascade.detectMultiScale(roi_gray)
+        mouthy = 0
+        for(mx,my,mw,mh) in mouth:
+            if my+y > y+(h/2):
+                cv2.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(0,0,255),2)
+                mouthy = my
+                break
+
+
+        nose = nose_cascade.detectMultiScale(roi_gray)
+        for(nx,ny,nw,nh) in nose:
+            if ny+nh < mouthy and ny > eyesy+eyesh:
+                cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(255,255,255),2)
+                break
 
     cv2.imshow('Skarface', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
