f65d2064ab328346e13dcc65226951bfdbfa6c15
Guillaume Petitpierre <guillaume.petitpierre@bluewin.ch>
2017-05-30 11:31:40 +0200

    Final version

--- skarface.py
+++ skarface.py
@@ -1,18 +1,25 @@
 import numpy as np
 import cv2
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+# Cascade Visage
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+
+# Cascade Yeux
 eye_cascade = cv2.CascadeClassifier('parojosG.xml')
 
+# Cascade Nez
 nose_cascade = cv2.CascadeClassifier('Nariz.xml')
+
+# Cascade Bouche
 mouth_cascade = cv2.CascadeClassifier('Mouth.xml')
 
-#img = cv2.imread("pics/dom.jpg")
+# Lecture de la webcam
 video_capture = cv2.VideoCapture(0)
+
+# Masques
+pics = ["pics/sunglasses.png", "pics/sunglasses2.png"]
+picdex = 0
+
 while True:
     ret, img = video_capture.read()
     b, g, r = cv2.split(img)
@@ -21,7 +28,7 @@ while True:
 
     img = cv2.merge((b, g, r, a))
 
-    sunglasses = cv2.imread("pics/sunglasses.png", flags=cv2.IMREAD_UNCHANGED)
+    sunglasses = cv2.imread(pics[picdex], flags=cv2.IMREAD_UNCHANGED)
 
     (sunB, sunG, sunR, sunA) = cv2.split(sunglasses)
     sunB = cv2.bitwise_and(sunB, sunB, mask=sunA)
@@ -34,7 +41,7 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
 
@@ -56,7 +63,7 @@ while True:
         mouthy = 0
         for(mx,my,mw,mh) in mouth:
             if my+y > y+(h/2):
-                cv2.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(0,0,255),2)
+                #cv2.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(0,0,255),2)
                 mouthy = my
                 break
 
@@ -64,12 +71,19 @@ while True:
         nose = nose_cascade.detectMultiScale(roi_gray)
         for(nx,ny,nw,nh) in nose:
             if ny+nh < mouthy and ny > eyesy+eyesh:
-                cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(255,255,255),2)
+                #cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(255,255,255),2)
                 break
 
     cv2.imshow('Skarface', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    a = cv2.waitKey(1)
+    if a & 0xFF == ord('q'):
         break
+    elif a & 0xFF == ord('x'):
+        # Change picture
+        if picdex+1 >= len(pics):
+            picdex = 0
+        else:
+            picdex += 1
 
 video_capture.release()
 cv2.destroyAllWindows()
